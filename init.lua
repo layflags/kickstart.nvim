a8d88869940a95272265ce0c88b5b2b0cf7f7c7b
Lay Flags <l@yfla.gs>
2025-04-09 15:10:42 +0200

    chore: add windsurf for AI assisted coding

--- init.lua
+++ init.lua
@@ -174,6 +174,29 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  -- Use Windsurf for AI assisted coding
+  {
+    'Exafunction/windsurf.vim',
+    event = 'BufEnter',
+    config = function()
+      vim.keymap.set('i', '<m-h>', function()
+        return vim.fn['codeium#AcceptNextWord']()
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<m-j>', function()
+        return vim.fn['codeium#CycleCompletions'](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<m-k>', function()
+        return vim.fn['codeium#CycleCompletions'](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<m-l>', function()
+        return vim.fn['codeium#AcceptNextLine']()
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<m-;>', function()
+        return vim.fn['codeium#Clear']()
+      end, { expr = true, silent = true })
+    end,
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
